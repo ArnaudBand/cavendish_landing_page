4a848a8053032f107b9525c95210bdda139a04c7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-24 23:07:53 +0000

    ✨ feat(App.js): add Chat component to App 🎉 feat(Chat.jsx): add Kommunicate chat widget to Chat component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Chat from './components/Chat';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Chat />
   );
 }
 
